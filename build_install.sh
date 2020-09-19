2f4643e946798d5209278b9fcd65e53fb616fd20
anadi <anadi.shukul@gmail.com>
2020-09-19 11:05:31 +0530

    travis clean 10

--- build_install.sh
+++ build_install.sh
@@ -14,6 +14,7 @@ mkdir -p $(PREFIX)
 which cmake
 set MAKE=`which cmake`
 echo $MAKE
-$(MAKE) -C build install
+#$(MAKE) -C build install
+g++ -std=c++14 -g tests/test1.cpp -I$(PREFIX)/include/uhdm -I$(PREFIX)/include/uhdm/include $(PREFIX)/lib/uhdm/libuhdm.a $(PREFIX)/lib/uhdm/libcapnp.a $(PREFIX)/lib/uhdm/libkj.a -ldl -lutil -lm -lrt -lpthread -o test_inst
 
 
