e694db3b29e691869efe94ebf3fa5ca69e58f894
anadi <anadi.shukul@gmail.com>
2020-09-19 10:54:35 +0530

    travis clean 8

--- build_install.sh
+++ build_install.sh
@@ -1,5 +1,16 @@
 # /bin/sh
-
+pwd
+which rm
 rm -f src/*
 rm -rf headers/*
 rm -rf build
+ls
+
+set PREFIX="/usr/local"
+mkdir -p build
+cd build; cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$(PREFIX)
+mkdir -p $(PREFIX)
+#	$(MAKE) -C build install
+which cmake
+set MAKE=`which cmake`
+
