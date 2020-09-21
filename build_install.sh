acd6fb1232a802e5626119dc3a34a1de922b17a4
anadi <anadi.shukul@gmail.com>
2020-09-21 13:57:49 +0530

    travis clean 14

--- build_install.sh
+++ build_install.sh
@@ -8,6 +8,7 @@ ls
 
 set PREFIX="/usr/local"
 echo $PREFIX
+#BUILD
 mkdir -p build
 cd build; cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
 mkdir -p $PREFIX
@@ -16,8 +17,12 @@ echo CMAKE VERSION
 which cmake
 set MAKE=`which cmake`
 echo "MAKE=$MAKE"
+#cd build && ctest --output-on-failure
 #$(MAKE) -C build install
 echo "G++ BUILD"
-g++ -std=c++14 -g tests/test1.cpp -I$PREFIX/include/uhdm -I$PREFIX/include/uhdm/include $PREFIX/lib/uhdm/libuhdm.a $PREFIX/lib/uhdm/libcapnp.a $PREFIX/lib/uhdm/libkj.a -ldl -lutil -lm -lrt -lpthread -o test_inst
+#TEST_INSTALL
+#g++ -std=c++14 -g tests/test1.cpp -I$PREFIX/include/uhdm -I$PREFIX/include/uhdm/include $PREFIX/lib/uhdm/libuhdm.a $PREFIX/lib/uhdm/libcapnp.a $PREFIX/lib/uhdm/libkj.a -ldl -lutil -lm -lrt -lpthread -o test_inst
+#./test_inst
+
 
 
