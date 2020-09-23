bc8596b10c5dffb3c25f2ac301e6dbcb645ffa63
anadi <anadi.shukul@gmail.com>
2020-09-23 09:30:42 +0530

    running_version 1

--- build_install.sh
+++ build_install.sh
@@ -1,25 +1,33 @@
 # /bin/sh
-pwd
-which rm
-rm -f src/*
-rm -rf headers/*
-rm -rf build
-ls
 
-set PREFIX="../install"
-echo $PREFIX
-#BUILD
-cmake --version
-which python
-which python3
-where python
-where python3
-mkdir -p build
-cd build
-echo "CMAKE at $PWD"
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
-cmake --build .
-echo "cmake build UnitTests"
-cmake --build . --target UnitTests
-echo "CTEST from here"
-#ctest --output-on-failure
+clean(){
+  echo "DO CLEAN"
+  rm -f src/*
+  rm -rf headers/*
+  rm -rf build
+}
+
+
+build(){
+  echo "MAKE BUILD"
+  set PREFIX="../install"
+  cmake --version
+  which python
+  which python3
+  where python
+  where python3
+  mkdir -p build
+  cd build
+  echo "CMAKE at $PWD"
+  cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
+  cmake --build .
+}
+
+tests(){
+  echo "Run CMAKE UnitTests"
+  cmake --build . --target UnitTests
+}
+
+clean
+build
+tests
